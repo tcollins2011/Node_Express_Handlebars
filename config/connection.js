11c5fbd396a3314f3af767ef4faa90f24a8f1d58
Tylre Collins <tyler@nhlp.com>
2020-05-12 09:50:49 -0600

    adding Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection 
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3308,
-  user: "root",
-  password: "K!rby0414",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3308,
+    user: "root",
+    password: "K!rby0414",
+    database: "burgers_db"
+})
+}
 
 // Make connection.
 connection.connect(function(err) {
